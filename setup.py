be95bcac75e1f19c1760ed23b7f5efef3e8cd2db
Jean Schurger <jean@schurger.org>
2013-03-14 15:43:08 -0400

    Removed installation of configuration sample in /etc to allow a simple user to install in its own env (eg: virrualenv). The example file, is now available in docs/

--- setup.py
+++ setup.py
@@ -12,13 +12,12 @@ from distutils.core import setup
 
 setup(
     name =          'py-Asterisk',
-    version =       '0.4',
+    version =       '0.4.1',
     description =   'Asterisk Manager API Python interface.',
     author =        'David Wilson',
     author_email =  'dw@botanicus.net',
     license =       'MIT',
     url =           'http://code.google.com/p/py-asterisk/',
     packages =      [ 'Asterisk' ],
-    scripts =       [ 'asterisk-dump', 'py-asterisk' ],
-    data_files =    [ ('/etc/asterisk', ['conf/py-asterisk.conf.sample']) ]
+    scripts =       [ 'asterisk-dump', 'py-asterisk' ]
 )
