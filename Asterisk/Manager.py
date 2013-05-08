27e3cadfcb14eade21705c2b7a7dd25c995ca285
David Wilson <dw@botanicus.net>
2013-05-08 23:28:13 +0100

    Add Asterisk 1.3 banner (patch by Alexis de Lattre)
        I just upgraded my Home Xivo server to version 13.08, which includes
        Asterisk 11 instead of Asterisk 1.8. I was surprised to see that my scripts
        that use py-asterisk no longer work. I figured out that it's just because
        the banner sent by AMI has changed to :
    
        Asterisk Call Manager/1.3
    
        I have patched py-asterisk to accept this new banner and my scripts that
        use py-asterisk work fine again with my Asterisk 11.3.0.
    
        Please find enclosed the patch.
    
        Regards,

--- Asterisk/Manager.py
+++ Asterisk/Manager.py
@@ -183,6 +183,7 @@ class BaseManager(Asterisk.Logging.InstanceLogger):
         'Asterisk Call Manager/1.0\r\n',
         'Asterisk Call Manager/1.1\r\n',
         'Asterisk Call Manager/1.2\r\n',
+        'Asterisk Call Manager/1.3\r\n',
     ]
 
 
